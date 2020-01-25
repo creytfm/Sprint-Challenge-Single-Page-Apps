34123bda393a3a30746957dafd9729be4632c427
Chris Reyes <creyop1987@gmail.com>
2020-01-25 11:36:49 -0600

    added api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import WelcomePage from "./components/WelcomePage";
+import { Route } from "react-router-dom";
+import List from "./components/List.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+        <Route className="route1" exact path="/" component={WelcomePage}/>
+        <Route className="route1" path="/List" component={List}/>
     </main>
   );
 }
+
